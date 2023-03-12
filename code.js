41577da6348c6766ec12a0b307f6f96c5756a9db
Danilo664 <danilomilosevic123@gmail.com>
2023-03-12 17:44:32 +0100

    added a dot functionality

--- code.js
+++ code.js
@@ -100,6 +100,7 @@ theDividing.addEventListener("click", ()=>{
 let equalBtn = document.getElementById("equals")
 equalBtn.addEventListener("click", equalize)
 
+
 let num1;
 let num2;
 let theText;
@@ -109,38 +110,60 @@ function equalize(){
             scndpara.textContent = "";
             alert("you can NOT divide by 0, choose other number")
         }else{
-            num1 = parseInt(mainpara.textContent);
-            num2 = parseInt(scndpara.textContent);
-            switch(whatWeDo){
-                case "adding":
-                    theText = num1 + num2;
-                    mainpara.textContent=theText.toString();
-                    scndpara.textContent ="";
-                    whatWeDo="";
-                    theSign.textContent="";
-                    break;
-                case "taking":
-                    theText = num1 - num2;
-                    mainpara.textContent=theText.toString();
-                    scndpara.textContent ="";
-                    whatWeDo="";
-                    theSign.textContent="";
-                    break;
-                case "multiplying":
-                    theText = num1 * num2;
-                    mainpara.textContent=theText.toString();
-                    scndpara.textContent ="";
-                    whatWeDo="";
-                    theSign.textContent="";
-                    break;
-                case "dividing":
-                    theText = num1 / num2;
-                    mainpara.textContent=theText.toString();
-                    scndpara.textContent ="";
-                    whatWeDo="";
-                    theSign.textContent="";
-                    break;
+            let firstDot = mainpara.textContent.split("")
+            firstDot = firstDot.pop()
+            let isItADot;
+            let scndDot = scndpara.textContent.split("")
+            scndDot = scndDot.pop()
+            if (firstDot ==="."){
+                isItADot = true;
+            }else if(scndDot ==="."){
+                isItADot = true;
+            }else{
+                isItADot = false;
             }
+            if(isItADot){
+                alert("Can not calculate this, you have dot at the end of one of you numbers")
+                alert("fix to countinue")
+            }else{
+                num1 = parseFloat(mainpara.textContent);
+                num2 = parseFloat(scndpara.textContent);
+                switch(whatWeDo){
+                    case "adding":
+                        theText = num1 + num2;
+                        theText = Math.round((theText + Number.EPSILON) * 100) / 100;
+                        mainpara.textContent=theText.toString();
+                        scndpara.textContent ="";
+                        whatWeDo="";
+                        theSign.textContent="";
+                        break;
+                    case "taking":
+                        theText = num1 - num2;
+                        theText = Math.round((theText + Number.EPSILON) * 100) / 100;
+                        mainpara.textContent=theText.toString();
+                        scndpara.textContent ="";
+                        whatWeDo="";
+                        theSign.textContent="";
+                        break;
+                    case "multiplying":
+                        theText = num1 * num2;
+                        theText = Math.round((theText + Number.EPSILON) * 100) / 100;
+                        mainpara.textContent=theText.toString();
+                        scndpara.textContent ="";
+                        whatWeDo="";
+                        theSign.textContent="";
+                        break;
+                    case "dividing":
+                        theText = num1 / num2;
+                        theText = Math.round((theText + Number.EPSILON) * 100) / 100;
+                        mainpara.textContent=theText.toString();
+                        scndpara.textContent ="";
+                        whatWeDo="";
+                        theSign.textContent="";
+                        break;
+                }
+            }
+
         }
     }/*calculates the numbers and outputs stuff into main para*/
 }
@@ -158,5 +181,22 @@ DelBtn.addEventListener("click", ()=>{
     nums.pop()
     nums = nums.join("")
     scndpara.textContent = nums;
+    }
+})
+
+let dotBtn = document.getElementById("dot")
+
+dotBtn.addEventListener("click", ()=>{
+    if(scndpara.textContent===""){
+        alert("You can not add just dot, you have to add a number!")
+        alert("Try again!")
+    }else{
+    let theText = scndpara.textContent.split("")
+    let hasTheDot = theText.includes(".")
+        if(hasTheDot){
+            alert("You can not add more than one dot in the value!")
+        }else{
+            scndpara.textContent += dotBtn.value
+        }
     }
 })
